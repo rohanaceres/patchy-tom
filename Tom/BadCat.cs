e5027425a31615e85482e9c5bc81263bd92e85ad
Ceres Carvalho | Stone <ccarvalho@stone.com.br>
2017-04-20 12:11:57 -0300

    :rocket: Adding bad cats to the world.

---
+++ Tom/BadCat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tom
+{
+    public sealed class BadCat
+    {
+        public static void PeeOnTheFloor()
+        {
+            Console.WriteLine("Peeing on the floor and not caring about it...");
+        }
+    }
+}
